7ca285b56973e2018da53ee7545d443fc7c7422b
Walter Lee <leewalter657@gmail.com>
2025-04-02 13:07:36 +0800

    update information

--- blockchain-backend/migrations/2_deploy_retailer.js
+++
@@ -1,5 +0,0 @@
-const Retailer = artifacts.require("Retailer");
-
-module.exports = function (deployer) {
-  deployer.deploy(Retailer);
-};
---
+++ blockchain-backend/migrations/deploy.js
@@ -0,0 +1,7 @@
+const AccountVerification = artifacts.require("AccountVerification");
+const SupplyChainManager = artifacts.require("SupplyChainManager");
+
+module.exports = function (deployer) {
+  deployer.deploy(AccountVerification);
+  deployer.deploy(SupplyChainManager);
+};

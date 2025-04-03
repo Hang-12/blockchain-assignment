ba7c7ba8f73a9668b644599fcd9da87e26b897c9
Hang-12 <sahzonghang@gmail.com>
2025-04-03 08:22:10 +0800

    updated async and await for contracts

--- blockchain-backend/migrations/1_deploy_contracts.js
+++ blockchain-backend/migrations/1_deploy_contracts.js
@@ -1,9 +1,15 @@
 const AccountVerification = artifacts.require("AccountVerification");
 const SupplyChainManager = artifacts.require("SupplyChainManager");
 
-module.exports = function (deployer) {
-  deployer.deploy(AccountVerification);
-  deployer.deploy(SupplyChainManager);
+module.exports = async function (deployer) {
+  // deployer.deploy(AccountVerification);
+  // deployer.deploy(SupplyChainManager);
+
+  await deployer.deploy(AccountVerification);
+    const accountVerifierInstance = await AccountVerification.deployed();
+
+    // Deploy SupplyChainManager with the address of AccountVerification
+    await deployer.deploy(SupplyChainManager, accountVerifierInstance.address);
 };
 
 
